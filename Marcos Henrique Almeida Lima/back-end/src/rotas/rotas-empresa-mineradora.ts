132e801c47a0672b7afd8005346fd76b4065ec4d
Glitcht0 <mh.almeidalima10@gmail.com>
2025-08-22 15:54:37 -0400

    psv3

--- Marcos Henrique Almeida Lima/back-end/src/rotas/rotas-empresa-mineradora.ts
+++ Marcos Henrique Almeida Lima/back-end/src/rotas/rotas-empresa-mineradora.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import verificarToken from "../middlewares/verificar-token";
 import verificarPerfilProfessor from "../middlewares/verificar-perfil-professor";
 import ServiçosProfessor from "../serviços/serviços-professor";
-const RotasProfessor = Router();
-export default RotasProfessor;
-RotasProfessor.post("/", ServiçosProfessor.cadastrarProfessor);
-RotasProfessor.get("/:cpf", verificarToken, verificarPerfilProfessor,
+const RotasGerentemineradora = Router();
+export default RotasGerentemineradora;
+RotasGerentemineradora.post("/", ServiçosProfessor.cadastrarProfessor);
+RotasGerentemineradora.get("/:cpf", verificarToken, verificarPerfilProfessor,
 ServiçosProfessor.buscarProfessor);
