75c1de14fe3c44310db2ca2ae12641a2576aece5
Glitcht0 <mh.almeidalima10@gmail.com>
2025-08-30 18:03:12 -0400

    erro

--- Marcos Henrique Almeida Lima/back-end/src/rotas/rotas-usuário.ts
+++ Marcos Henrique Almeida Lima/back-end/src/rotas/rotas-usuário.ts
@@ -3,9 +3,11 @@ import ServiçosUsuário from "../serviços/serviços-usuário";
 const RotasUsuário = Router();
 export default RotasUsuário;
 RotasUsuário.post("/login", (req, res, next) => {
-    console.log("Rota de login acessada. Tentando logar usuário.");
+    // Adicione este log para confirmar que a rota foi acessada
+    console.log("Rota de login acessada.");
     next();
 }, ServiçosUsuário.logarUsuário);
+
 RotasUsuário.get("/verificar-cpf-existente/:cpf", (req, res) => {
     console.log("Rota de verificação de CPF acessada.");
     ServiçosUsuário.verificarCpfExistente(req, res);
