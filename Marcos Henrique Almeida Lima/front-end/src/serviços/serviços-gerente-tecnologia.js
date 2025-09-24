16bf622fdbbab0089b6d6d6972670dfc7c60a1f5
Glitcht0 <mh.almeidalima10@gmail.com>
2025-09-23 20:12:37 -0400

    quero botar fogo nisso

--- Marcos Henrique Almeida Lima/front-end/src/serviços/serviços-gerente-tecnologia.js
+++ Marcos Henrique Almeida Lima/front-end/src/serviços/serviços-gerente-tecnologia.js
@@ -1,7 +1,7 @@
 import servidor from "./servidor";
-export function serviçoCadastrarGerenteTecnologia(gerente) { return servidor.post("/gerentes-tecnologia", gerente); };
-export function serviçoAtualizarGerenteTecnologia(gerente) { return servidor.patch("/gerentes-tecnologia", gerente); };
-export function serviçoBuscarGerenteTecnologia(cpf) { return servidor.get(`/gerentes-tecnologia/${cpf}`); }
+export function serviçoCadastrarGerenteTecnologia(gerente) { return servidor.post("/gerente-tecnologia", gerente); };
+export function serviçoAtualizarGerenteTecnologia(gerente) { return servidor.patch("/gerente-tecnologia", gerente); };
+export function serviçoBuscarGerenteTecnologia(cpf) { return servidor.get(`/gerente-tecnologia/${cpf}`); }
 
 
 
